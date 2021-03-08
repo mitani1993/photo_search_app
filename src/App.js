a2be4260d4b6bd92e84228795e6e86162623240e
mitani1993 <fytim55@gmail.com>
2021-03-08 14:48:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [text, setText] = useState('');
+  const [query, setQuery] = useState('apple');
+
+  useEffect(() => {
+    console.log('api');
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setImages(data.results)
+      })
+  }, [query]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(text);
+    setText('');
+    console.log("onSubmit");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <form onSubmit={onSubmit}>
+          <input type="text" 
+            onChange={e => setText(e.target.value)}
+            value={text}
+          />
+          <button type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+      <div className="container">
+        {
+          images.map(image => (
+            <div key={image.id} className="card">
+              <img src={image.urls.regular} className="card-img" alt="" />
+              <div className="card-content">
+                <div className="card-title">
+                  {image.alt_description}
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
